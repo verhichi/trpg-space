271412a0d9c64c5767452100934203f183d55a62
Daichi Nishida <verhichi@hotmail.com>
2018-12-21 15:59:59 +0900

    Add Routing component and Switch to body component. Create lobby component.
    -

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
+// Routing
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
+
 // Style
 import './body.scss';
+
+// Components
+import Lobby from './lobby/lobby';
 
 class Body extends Component {
   render() {
     return (
       <main>
-        BODY WORKS!
+        <Router>
+          <Switch>
+            <Route component={Lobby} />
+          </Switch>
+        </Router>
       </main>
     );
   }
---
+++ src/components/body/lobby/lobby.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// Font Awesome Component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Style
+import './lobby.scss';
+
+class Lobby extends Component {
+  render() {
+    return (
+      <div className="lobby-cont h-100">
+        LOBBY WORKS!
+      </div>
+    );
+  }
+}
+
+export default Lobby;
