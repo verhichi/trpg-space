d48523126e323e8d061defb668f9317335ff1faf
Daichi Nishida <verhichi@hotmail.com>
2018-12-24 23:36:11 +0900

    Add place holder express and socket.io logic.
    -

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');           // import express framework
+const app     = express();                    // instantiating express
+const http    = require('http');              // import http package
+const server  = http.Server(app);             // Create server
+const io      = require('socket.io')(server); // set socket.io with server
+
+const portNo  = process.env.PORT || 3000; // set port number
+
+const path = require('path');            // import path object
+
+const bodyParser = require('body-parser');        // import body-parser
+app.use(bodyParser.urlencoded({extended: true})); // use body-parser
+app.use(bodyParser.json());                       // parse body to json
+
+// Set express listener at port 3000
+server.listen(portNo, () => {
+  const serverStartTimestamp = new Date().toLocaleString();
+
+  console.log('\n--- SERVER STATUS ---');
+  console.log('Server Start Date:', serverStartTimestamp);
+
+  console.log('The Server is up and running!');
+  console.log(`Access the website at: http://localhost:${portNo}`);
+});
+
+// Set path for file to ./dist
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Place Holder socket.io logic
+io.on('connection', (socket) => {
+
+  // Logic for when a new user joins the room
+  socket.on('join', (room_id) => {
+    for (let room in socket.rooms){
+      socket.leave(room);
+    }
+    socket.join(room_id);
+  });
+
+});
