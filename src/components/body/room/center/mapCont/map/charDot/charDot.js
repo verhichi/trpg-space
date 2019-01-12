9cf98e2a99cfc14c5e7abebb8bbc94048c4d4553
Daichi Nishida <verhichi@hotmail.com>
2019-01-13 04:22:09 +0900

    Add character info balloon that displays when user hovers over character dot.
    -

--- src/components/body/room/center/mapCont/map/charDot/charDot.js
+++ src/components/body/room/center/mapCont/map/charDot/charDot.js
@@ -25,10 +25,10 @@ const mapStateToProps = (state) => {
 // Redux Map Dispatch To Props
 const mapDispatchToProps = (dispatch) => {
   return {
-    setMapMode: (mode) => dispatch(setMapMode(mode)),
-    addMapChar: (charData) => dispatch(addMapChar(charData)),
-    editMapChar: (charData) => dispatch(editMapChar(charData)),
-    setCharToPlace: (charId) => dispatch(setCharToPlace(charId))
+    setMapMode:     (mode)     => dispatch(setMapMode(mode)),
+    addMapChar:     (charData) => dispatch(addMapChar(charData)),
+    editMapChar:    (charData) => dispatch(editMapChar(charData)),
+    setCharToPlace: (charId)   => dispatch(setCharToPlace(charId))
   };
 };
 
@@ -36,7 +36,12 @@ class CharDot extends Component {
 
   render() {
     return (
-      <div className="map-char cursor-pointer" key={this.props.charData.charId} style={{backgroundColor: this.props.charData.color, left: this.props.charData.mapCoor.x, top: this.props.charData.mapCoor.y}}>
+      <div className="map-char p-relative cursor-pointer" key={this.props.charData.charId} style={{backgroundColor: this.props.charData.color, left: this.props.charData.mapCoor.x, top: this.props.charData.mapCoor.y}}>
+        <div className="map-char-balloon p-absolute p-1 align-left">
+          <div className="font-size-md font-weight-bold pb-1 one-line-ellipsis">{this.props.charData.name}</div>
+          <div className="font-size-sm one-line-ellipsis"><FontAwesomeIcon icon="heart"/> {this.props.charData.curHp} / {this.props.charData.maxHp}</div>
+          <div className="font-size-sm one-line-ellipsis"><FontAwesomeIcon icon="flask"/> {this.props.charData.curMp} / {this.props.charData.maxMp}</div>
+        </div>
       </div>
     );
   }
