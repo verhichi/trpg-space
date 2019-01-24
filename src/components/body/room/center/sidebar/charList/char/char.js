4d3a98e2c8a0073c6498cf38ce19e7b1900fd3a6
Daichi Nishida <verhichi@hotmail.com>
2019-01-24 16:48:08 +0900

    Edit char component to display the owner's name for each character.
    -

--- src/components/body/room/center/sidebar/charList/char/char.js
+++ src/components/body/room/center/sidebar/charList/char/char.js
@@ -13,7 +13,8 @@ import './char.scss';
 const mapStateToProps = (state) => {
   return {
     id: state.id,
-    roomId: state.roomId
+    roomId: state.roomId,
+    userList: state.userList
   };
 };
 
@@ -71,6 +72,7 @@ class Char extends Component {
     const showStat = this.props.charData.general.privacy <= 0 || this.props.charData.ownerId === this.props.id;
 
     const charName = showName ? this.props.charData.general.name : 'UNKNOWN';
+    const userName = this.props.userList.find(user => user.id === this.props.charData.ownerId).name;
 
     const statList = this.props.charData.status.map(status => {
       if (status.type === 'value'){
@@ -88,7 +90,8 @@ class Char extends Component {
       <div className="char-cont d-flex" style={{background: `linear-gradient(135deg, ${this.props.charData.general.color} 10%, #fff 0)`}}>
         <div className="char-profile-circle f-shrink-0" style={imageStyle}></div>
         <div className="char-data-cont d-flex f-dir-col f-grow-1 pl-1 pt-1 pb-1">
-          <div className="char-name font-weight-bold pb-1">{charName}</div>
+          <div className="char-owner one-line-ellipsis font-size-sm font-weight-bold">{userName}</div>
+          <div className="one-line-ellipsis font-weight-bold pb-1">{charName}</div>
           { statList }
         </div>
         <div className="d-flex f-dir-col f-shrink-0 pr-1 pt-1">
@@ -114,31 +117,5 @@ class Char extends Component {
     );
   }
 }
-
-// <div className="char-cont w-100" style={{background: `linear-gradient(135deg, #fff 85%, ${this.props.charData.general.color} 0)`}}>
-//   <div className="char-head d-flex mb-3">
-//     {this.props.charData.ownerId === this.props.id
-//       ? (<div className="pr-1 cursor-pointer char-btn align-center f-shrink-0" onClick={this.handleEditClick}>
-//            <FontAwesomeIcon icon="pen-square"/>
-//          </div>)
-//       : (<div className="cursor-pointer char-btn align-center f-shrink-0" onClick={this.handleViewClick}>
-//            <FontAwesomeIcon icon="eye"/>
-//          </div>)}
-//     <div className="char-name f-grow-1 font-weight-bold">{charName}</div>
-//     {this.props.charData.general.link.length !== 0
-//       ? (<a className="char-btn text-dec-none align-center f-shrink-0 remove-link-dec" href={this.props.charData.general.link} target="_blank" rel='noreferrer noopener'>
-//            <div className="cursor-pointer"><FontAwesomeIcon icon="link"/></div>
-//          </a>)
-//       : null}
-//     {this.props.charData.ownerId === this.props.id
-//       ? (<div className="cursor-pointer char-btn align-center f-shrink-0" onClick={this.handleRemoveClick}>
-//            <FontAwesomeIcon icon="window-close"/>
-//          </div>)
-//       : null}
-//   </div>
-//   <div className="char-body">
-//     {statList}
-//   </div>
-// </div>
 
 export default connect(mapStateToProps, mapDispatchToProps)(Char);
