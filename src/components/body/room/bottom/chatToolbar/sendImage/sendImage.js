00eb18a99673f5245e7668fd9774c07ee791b0f7
Daichi Nishida <verhichi@hotmail.com>
2019-01-26 03:20:36 +0900

    Create sendImage component to increase granularity of chatToolbar component.
    -

---
+++ src/components/body/room/bottom/chatToolbar/sendImage/sendImage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { showModal } from '../../../../../../redux/actions/action';
+
+// Style
+import './sendImage.scss';
+
+// Font Awesome Component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Redux Map Dispatch To Props
+const mapDispatchToProps = (dispatch) => {
+  return { showModal: (modalType, modalProp) => dispatch(showModal(modalType, modalProp)) };
+};
+
+class ChatToolbar extends Component {
+  constructor (props){
+    super(props);
+    this.handleImageClick = this.handleImageClick.bind(this);
+  }
+
+  handleImageClick (e){
+    e.preventDefault(e);
+
+    this.props.showModal('uploadImg', {
+      title: 'Upload an image',
+      displayClose: true,
+      type: 'chat'
+    });
+  }
+
+  render (){
+    return(
+      <div className="chat-opt-btn" onClick={this.handleImageClick}>
+        <FontAwesomeIcon icon="paperclip"/>
+      </div>
+    );
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ChatToolbar);
