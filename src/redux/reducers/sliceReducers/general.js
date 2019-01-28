ac93c983b33f420d16ae39680cdf5cf41887c222
Daichi Nishida <verhichi@hotmail.com>
2019-01-28 16:00:58 +0900

    Create sliceReducers folder to hold slice reducers. Create general.js inside sliceReducers folder to hold reducers for general reducer logic.
    -

---
+++ src/redux/reducers/sliceReducers/general.js
@@ -0,0 +1,29 @@
+import { SET_ROOM_ID, SET_USER_ID } from '../../../constants/actionTypes';
+
+const initialState = {
+  isMobile: /Mobile/.test(navigator.userAgent),
+  id: '',
+  roomId: ''
+};
+
+const generalReducer = (state = initialState, action) => {
+  switch(action.type){
+    case SET_ROOM_ID:
+      return {
+        ...state,
+        roomId: action.roomId
+      };
+
+    case SET_USER_ID:
+      return {
+        ...state,
+        id: action.userId
+      };
+
+    default:
+      return state;
+    }
+
+};
+
+export default generalReducer;
