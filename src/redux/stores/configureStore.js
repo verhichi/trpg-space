fff0d2e446e73ce954e117a6a30e8b6a76117df9
Daichi Nishida <verhichi@hotmail.com>
2019-01-28 15:52:20 +0900

    Rename reducer.js to rootReducer.js. Edit store file to import from renamed path.
    -

--- src/redux/stores/configureStore.js
+++ src/redux/stores/configureStore.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
-import rootReducer, { initialState } from '../reducers/reducer';
+import rootReducer, { initialState } from '../reducers/rootReducer';
 
 export const store = createStore(rootReducer, initialState);
