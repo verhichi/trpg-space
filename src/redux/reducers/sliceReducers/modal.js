e6b9d4b03ac0ff475ab81e1df3c53fb913504a73
Daichi Nishida <verhichi@hotmail.com>
2019-01-28 16:05:52 +0900

    Create modal.js inside sliceReducers folder to hold reducers for modal reducer logic.
    -

---
+++ src/redux/reducers/sliceReducers/modal.js
@@ -0,0 +1,36 @@
+import { SHOW_MODAL, HIDE_MODAL } from '../../../constants/actionTypes';
+
+const initialState = {
+  display: false,
+  modalType: '',
+  modalProp: {}
+  //   {
+  //   *required
+  //   title*: header title of modal
+  //   displayClose*: display the X button on the top-right to close modal
+  // }
+};
+
+const generalReducer = (state = initialState, action) => {
+  switch(action.type){
+    case SHOW_MODAL:
+      return {
+        display:   true,
+        modalType: action.modalType,
+        modalProp: action.modalProp
+      };
+
+    case HIDE_MODAL:
+      return {
+        display:   false,
+        modalType: '',
+        modalProp: {}
+      };
+
+    default:
+      return state;
+    }
+
+};
+
+export default generalReducer;
