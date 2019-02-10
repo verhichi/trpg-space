b3168bce6995d6d6b7109585b87613f127b5eea7
Daichi Nishida <verhichi@hotmail.com>
2019-02-10 16:37:49 +0900

    Organized values in actionTypes file.
    -

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,61 +1,80 @@
-export const ADD_USER= 'ADD_USER';
-export const ADD_TO_CHAT_LOG = 'ADD_TO_CHAT_LOG';
-export const ADD_CHAT = 'ADD_CHAT';
-export const ADD_TO_CHAR_LIST = 'ADD_TO_CHAR_LIST';
-export const ADD_CHAR = 'ADD_CHAR';
-export const EDIT_CHAR = 'EDIT_CHAR';
-export const EDIT_USER = 'EDIT_USER';
-export const HIDE_SIDEBAR = 'HIDE_SIDEBAR';
-export const HIDE_MODAL = 'HIDE_MODAL';
-export const HIDE_USER_LIST = 'HIDE_USER_LIST';
-export const REMOVE_FROM_CHAR_LIST = 'REMOVE_FROM_CHAR_LIST';
-export const REMOVE_CHAR = 'REMOVE_CHAR';
-export const REMOVE_USER = 'REMOVE_USER';
-export const SET_ROOM_ID = 'SET_ROOM_ID';
-export const SET_USER_ID = 'SET_USER_ID';
-export const SHOW_SIDEBAR = 'SHOW_SIDEBAR';
-export const SHOW_MODAL = 'SHOW_MODAL';
-export const SHOW_USER_LIST = 'SHOW_USER_LIST';
-export const SHOW_DICE_BUBBLE = 'SHOW_DICE_BUBBLE';
-export const HIDE_DICE_BUBBLE = 'HIDE_DICE_BUBBLE';
-export const USER_CLEANUP = 'USER_CLEANUP';
-export const NEW_HOST = 'NEW_HOST';
-export const SHOW_CHAT = 'SHOW_CHAT';
-export const SHOW_MAP = 'SHOW_MAP';
-export const EDIT_MAP_IMAGE = 'EDIT_MAP_IMAGE';
-export const SET_MAP_MODE = 'SET_MAP_MODE';
-export const ADD_MAP_CHAR = 'ADD_MAP_CHAR';
-export const EDIT_MAP_CHAR = 'EDIT_MAP_CHAR';
-export const REMOVE_MAP_CHAR = 'REMOVE_MAP_CHAR';
-export const SHOW_PLACE_CHAR = 'SHOW_PLACE_CHAR';
-export const HIDE_PLACE_CHAR = 'HIDE_PLACE_CHAR';
-export const SHOW_REMOVE_CHAR = 'SHOW_REMOVE_CHAR';
-export const HIDE_REMOVE_CHAR = 'HIDE_REMOVE_CHAR';
-export const TOGGLE_REMOVE_CHAR = 'TOGGLE_REMOVE_CHAR';
-export const SET_CHAR_TO_PLACE = 'SET_CHAR_TO_PLACE';
-export const TOGGLE_MAP_GRID = 'TOGGLE_MAP_GRID';
-export const SET_SIDEBAR_TAB_MODE = 'SET_SIDEBAR_TAB_MODE';
-export const LOCK_NOTE = 'LOCK_NOTE';
-export const UNLOCK_NOTE = 'UNLOCK_NOTE';
-export const EDIT_NOTE = 'EDIT_NOTE';
-export const EDIT_MAP_POSITION = 'EDIT_MAP_POSITION';
-export const EDIT_MAP_SCALE = 'EDIT_MAP_SCALE';
-export const SHOW_MAP_SCALE = 'SHOW_MAP_SCALE';
-export const HIDE_MAP_SCALE = 'HIDE_MAP_SCALE';
-export const CHECK_SEND_MSG_TO_ALL = 'CHECK_SEND_MSG_TO_ALL';
-export const UNCHECK_SEND_MSG_TO_ALL = 'UNCHECK_SEND_MSG_TO_ALL';
-export const ADD_SEND_MSG_USER = 'ADD_SEND_MSG_USER';
-export const REMOVE_SEND_MSG_USER = 'REMOVE_SEND_MSG_USER';
-export const EDIT_SEND_AS = 'EDIT_SEND_AS';
-export const CHECK_SEND_AS_USER = 'CHECK_SEND_AS_USER';
-export const UNCHECK_SEND_AS_USER = 'UNCHECK_SEND_AS_USER';
-export const SET_SIDEBAR_CHAR = 'SET_SIDEBAR_CHAR';
-export const SET_SIDEBAR_NOTE = 'SET_SIDEBAR_NOTE';
-export const SET_APP_LANG = 'SET_APP_LANG';
 export const RESET_STATE = 'RESET_STATE';
-export const ADD_MAP = 'ADD_MAP';
-export const EDIT_MAP = 'EDIT_MAP';
-export const REMOVE_MAP = 'REMOVE_MAP';
-export const SET_DISPLAY_MAP = 'SET_DISPLAY_MAP';
+
+// Global
+export const SET_ROOM_ID  = 'SET_ROOM_ID';
+export const SET_USER_ID  = 'SET_USER_ID';
+export const SET_APP_LANG = 'SET_APP_LANG';
+
+// User
+export const ADD_USER    = 'ADD_USER';
+export const EDIT_USER   = 'EDIT_USER';
+export const REMOVE_USER = 'REMOVE_USER';
+export const NEW_HOST    = 'NEW_HOST';
+
+// Character
+export const ADD_CHAR    = 'ADD_CHAR';
+export const EDIT_CHAR   = 'EDIT_CHAR';
+export const REMOVE_CHAR = 'REMOVE_CHAR';
+
+// Map
+export const ADD_MAP           = 'ADD_MAP';
+export const EDIT_MAP          = 'EDIT_MAP';
+export const REMOVE_MAP        = 'REMOVE_MAP';
+export const EDIT_MAP_POSITION = 'EDIT_MAP_POSITION';
+export const EDIT_MAP_SCALE    = 'EDIT_MAP_SCALE';
+export const SET_MAP_MODE      = 'SET_MAP_MODE';
+export const SET_CHAR_TO_PLACE = 'SET_CHAR_TO_PLACE';
+export const TOGGLE_MAP_GRID   = 'TOGGLE_MAP_GRID';
+
+// MapChar
+export const ADD_MAP_CHAR                 = 'ADD_MAP_CHAR';
+export const EDIT_MAP_CHAR                = 'EDIT_MAP_CHAR';
+export const REMOVE_MAP_CHAR              = 'REMOVE_MAP_CHAR';
 export const REMOVE_ALL_CHAR_FROM_SEL_MAP = 'REMOVE_ALL_CHAR_FROM_SEL_MAP';
 export const REMOVE_SEL_CHAR_FROM_ALL_MAP = 'REMOVE_SEL_CHAR_FROM_ALL_MAP';
+
+// Chat
+export const ADD_CHAT = 'ADD_CHAT';
+
+// Chat Setting
+export const CHECK_SEND_MSG_TO_ALL   = 'CHECK_SEND_MSG_TO_ALL';
+export const UNCHECK_SEND_MSG_TO_ALL = 'UNCHECK_SEND_MSG_TO_ALL';
+export const ADD_SEND_MSG_USER       = 'ADD_SEND_MSG_USER';
+export const REMOVE_SEND_MSG_USER    = 'REMOVE_SEND_MSG_USER';
+export const EDIT_SEND_AS            = 'EDIT_SEND_AS';
+export const CHECK_SEND_AS_USER      = 'CHECK_SEND_AS_USER';
+export const UNCHECK_SEND_AS_USER    = 'UNCHECK_SEND_AS_USER';
+
+// Note
+export const LOCK_NOTE   = 'LOCK_NOTE';
+export const UNLOCK_NOTE = 'UNLOCK_NOTE';
+export const EDIT_NOTE   = 'EDIT_NOTE';
+
+// Modal
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const HIDE_MODAL = 'HIDE_MODAL';
+
+// Display
+export const SHOW_SIDEBAR     = 'SHOW_SIDEBAR';
+export const HIDE_SIDEBAR     = 'HIDE_SIDEBAR';
+export const SHOW_DICE_BUBBLE = 'SHOW_DICE_BUBBLE';
+export const HIDE_DICE_BUBBLE = 'HIDE_DICE_BUBBLE';
+export const SHOW_CHAT        = 'SHOW_CHAT';
+export const SHOW_MAP         = 'SHOW_MAP';
+export const SHOW_PLACE_CHAR  = 'SHOW_PLACE_CHAR';
+export const HIDE_PLACE_CHAR  = 'HIDE_PLACE_CHAR';
+export const SHOW_REMOVE_CHAR = 'SHOW_REMOVE_CHAR';
+export const HIDE_REMOVE_CHAR = 'HIDE_REMOVE_CHAR';
+export const SHOW_MAP_SCALE   = 'SHOW_MAP_SCALE';
+export const HIDE_MAP_SCALE   = 'HIDE_MAP_SCALE';
+export const SET_SIDEBAR_CHAR = 'SET_SIDEBAR_CHAR';
+export const SET_SIDEBAR_NOTE = 'SET_SIDEBAR_NOTE';
+export const SET_DISPLAY_MAP  = 'SET_DISPLAY_MAP';
+
+// export const HIDE_USER_LIST = 'HIDE_USER_LIST';
+// export const SHOW_USER_LIST = 'SHOW_USER_LIST';
+//
+// export const USER_CLEANUP = 'USER_CLEANUP';
+// export const EDIT_MAP_IMAGE = 'EDIT_MAP_IMAGE';
+// export const TOGGLE_REMOVE_CHAR = 'TOGGLE_REMOVE_CHAR';
+// export const SET_SIDEBAR_TAB_MODE = 'SET_SIDEBAR_TAB_MODE';
