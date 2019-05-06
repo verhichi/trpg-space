5790fdf6a7e9fe668d6c9055072ba7813132cfc5
Daichi Nishida <verhichi@hotmail.com>
2019-05-06 12:38:56 +0900

    Create loader component to display while page view loads. Add Lazy load feature for Body component's routing.
    -

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 
 // Routing
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,19 +7,22 @@ import { Switch, Route } from 'react-router';
 // Style
 import './body.scss';
 
-// Components
-import Lobby from './lobby/lobby';
-import Room from './room/room';
+// // Components
+import Loader from './loader/loader';
+const Lobby = (lazy(() => (import('./lobby/lobby'))));
+const Room = (lazy(() => (import('./room/room'))));
 
 class Body extends Component {
   render() {
     return (
       <main>
         <Router>
-          <Switch>
-            <Route exact path="/"  component={Lobby} />
-            <Route exact path="/:roomId" component={Room} />
-          </Switch>
+          <Suspense fallback={<Loader/>}>
+            <Switch>
+              <Route exact path="/"  component={Lobby} />
+              <Route exact path="/:roomId" component={Room} />
+            </Switch>
+          </Suspense>
         </Router>
       </main>
     );
