21e2c60423295a97be1a6a9d08d71a0bcaa6e2a7
Daichi Nishida <verhichi@hotmail.com>
2019-11-02 16:49:41 +0900

    fix: fix image modal to display image properly.

--- src/components/modal/image/image.js
+++ src/components/modal/image/image.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { hideModal } from '../../../redux/actions/modal';
+
+
+// Font Awesome Component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // Style
 import './image.scss';
@@ -9,14 +14,36 @@ const mapStateToProps = (state) => {
   return { modalSetting: state.modalSetting };
 };
 
+// Redux Map Dispatch To Props
+const mapDispatchToProps = (dispatch) => {
+  return { hideModal: () => dispatch(hideModal()) };
+};
+
+
 class Image extends Component {
+  constructor (props){
+    super(props);
+    this.handleCloseClick = this.handleCloseClick.bind(this);
+  }
+
+  handleCloseClick (){
+    this.props.hideModal();
+  }
+
   render() {
     return (
-      <div className="d-flex f-grow-1 f-align-items-center justify-content-center">
-        <img className="modal-img" src={this.props.modalSetting.modalProp.src}/>
+      <div className="h-100 w-100">
+        <div className="image-modal-toolbar w-100 font-size-xxl align-right">
+          <span className="cursor-pointer" onClick={this.handleCloseClick}>
+            <FontAwesomeIcon icon="times"/>
+          </span>
+        </div>
+        <div className="modal-img-cont h-100 w-100 d-flex f-align-items-center justify-content-center overflow-auto">
+          <img className="modal-img p-relative" src={this.props.modalSetting.modalProp.src}/>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps)(Image);
+export default connect(mapStateToProps, mapDispatchToProps)(Image);
